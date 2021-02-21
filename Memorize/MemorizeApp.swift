a205d0237a91a775c479889f80d20f4b282da812
Andre <andre.coutinho@bernoulli.com.br>
2021-02-21 14:23:21 -0300

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Andre Luis Barbosa Coutinho on 21/02/21.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
